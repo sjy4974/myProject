eb8e201eb880bb469c951e5d8ac7ef1124cd3909
ssjy4974 <ssjy4974@naver.com>
2022-04-21 21:17:25 +0900

    게시판 CRUD구현

---
+++ src/main/java/com/myProject/springboot/web/web/dto/PostsListResponseDto.java
@@ -0,0 +1,22 @@
+package com.myProject.springboot.web.web.dto;
+
+import com.myProject.springboot.web.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+
+}
