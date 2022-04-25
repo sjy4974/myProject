150d7f92df062b62c6caf3651cc8cd83ca27758a
ssjy4974 <ssjy4974@naver.com>
2022-04-25 21:56:52 +0900

    구글 로그인 구현, 스프링 시큐리티 구현과 그로인한 테스트 코드 수정

--- src/test/java/com/myProject/springboot/web/HelloControllerTest.java
+++ src/test/java/com/myProject/springboot/web/HelloControllerTest.java
@@ -1,10 +1,14 @@
 package com.myProject.springboot.web;
 
+import com.myProject.springboot.web.config.auth.SecurityConfig;
 import com.myProject.springboot.web.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,14 +20,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles = "USER")
     @Test
-    public void hello() throws Exception{
+    public void hello가_리턴된다() throws Exception{
         String hello = "hello";
 
         mvc.perform(get("/hello"))
@@ -31,6 +40,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto가_리턴된다() throws Exception{
         String name = "hello";
